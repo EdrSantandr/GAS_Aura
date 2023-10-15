5c9843e6ccbec8785f94c16a3a971af1735a53bf
Eder Santander <eder@bamtang.com>
2023-10-15 18:30:40 -0500

    HighlightLogic effect with material, remember on project settings. Post process enabled with Depth

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,13 +2,34 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
-	bHighlighted = true;
+	//First the Skeletal mesh
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+
+	//The weapon
+	if (Weapon)
+	{
+		Weapon->SetRenderCustomDepth(true);
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
+	//First the Skeletal mesh
+	GetMesh()->SetRenderCustomDepth(false);
+	//The weapon
+	if (Weapon)
+	{
+		Weapon->SetRenderCustomDepth(false);
+	}
 }
