cbb4ce54a79170808bd50958323e406c6eaf0657
Eder Santander <eder@bamtang.com>
2023-10-16 06:50:43 -0500

    PlayerState class created to handle Player side of GAS

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// Copyright Eder
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	//How often the server update clients for other games like fornite this value is higher
+	NetUpdateFrequency = 100.f;
+}
