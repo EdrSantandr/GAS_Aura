1afc5d39802fe5b3684718643b523b6143c30e89
Eder Santander <eder@bamtang.com>
2023-10-20 10:05:22 -0500

    Added a reference to DataTable to retrieve messages from a datatable to thw OverlayWidgetController

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -6,11 +6,32 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
+class UAuraUserWidget;
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
 // IMPORTANT STEPS (1) setup delegates
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+
+USTRUCT(BlueprintType)
+struct FUIWidgetRow :public  FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayTag MessageTag = FGameplayTag();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText Message = FText();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<UAuraUserWidget> MessageWidget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UTexture2D* Image = nullptr;
+};
+
+
 /**
  * 
  */
@@ -38,6 +59,10 @@ public:
 	FOnMaxManaChangedSignature OnMaxManaChangedSignature;
 
 protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
+	TObjectPtr<UDataTable> MessageWidgetDataTable;
+	
 	void HealthChange(const FOnAttributeChangeData& Data) const;
 
 	void MaxHealthChange(const FOnAttributeChangeData& Data) const;
@@ -46,4 +71,5 @@ protected:
 	void ManaChange(const FOnAttributeChangeData& Data) const;
 
 	void MaxManaChange(const FOnAttributeChangeData& Data) const;
+	
 };
