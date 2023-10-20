5986c5d87e9fbe90c7df10b217bcf4a656f4f31b
Eder Santander <eder@bamtang.com>
2023-10-20 09:36:26 -0500

    Showing all tags granted to a Gameplayeffect

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -11,5 +11,14 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 void UAuraAbilitySystemComponent::EffectApply(UAbilitySystemComponent* AbilitySystemComponent,
                                               const FGameplayEffectSpec& GameplayEffectSpec, FActiveGameplayEffectHandle ActiveGameplayEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue,FString("Effect Applied"));
+	//GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue,FString("Effect Applied"));
+	FGameplayTagContainer GameplayTagContainer;
+	GameplayEffectSpec.GetAllAssetTags(GameplayTagContainer);
+
+	for(const FGameplayTag& Tag: GameplayTagContainer)
+	{
+		//TODO: Broadcast the tag to the widget controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue,Msg);
+	}
 }
