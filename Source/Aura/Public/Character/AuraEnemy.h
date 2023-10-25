5d80217ebac8efba6a922926c9100b0b0a6ed646
Eder Santander <eder@bamtang.com>
2023-10-25 09:04:43 -0500

    Added combat interface to handle level on player state and enemies. Both handle Level in diferent ways so can be replicated for clients and playerstate for the server

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -24,8 +24,15 @@ public:
 	virtual void UnHighlightActor() override;
 	// END Enemy interface
 
+	// Combat interface
+	virtual int32 GetPlayerLevel() override; 
+	// End Combat interface
+
 protected:
 	virtual void BeginPlay() override;
 
 	virtual void InitAbilityActorInfo() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character Class Defaults")
+	int32 Level = 1;
 };
