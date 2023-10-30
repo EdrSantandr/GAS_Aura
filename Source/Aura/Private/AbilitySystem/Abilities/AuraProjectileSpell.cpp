d8acedcfa717236eba5a21f011d43b9973b42afa
Eder Santander <eder@bamtang.com>
2023-10-30 09:17:25 -0500

    Created a custom gameplay ability class so we can handle specific behavior

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,16 @@
+// Copyright Eder
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, "Activate ability c++ ", true, true,FLinearColor::Blue,3.f);
+	
+}
