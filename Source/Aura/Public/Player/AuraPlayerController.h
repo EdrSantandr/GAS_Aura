57886c638c58c46f6f779dcf60315c920f401549
Eder Santander <eder@bamtang.com>
2023-10-30 06:48:45 -0500

    Impelementing hold and move to location for multiplayer

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -7,6 +7,7 @@
 #include "GameplayTagContainer.h"
 #include "AuraPlayerController.generated.h"
 
+class USplineComponent;
 class UAuraAbilitySystemComponent;
 struct FGameplayTag;
 class UAuraInputConfig;
@@ -57,4 +58,21 @@ private:
 
 	UFUNCTION()
 	UAuraAbilitySystemComponent* GetASC();
+
+	FVector CachedDestination = FVector::ZeroVector;
+
+	float FollowTime = 0.f;
+
+	float ShortPressThreshold = 0.5f;
+
+	bool bAutoRunning = false;
+
+	bool bTargeting = false;
+
+	UPROPERTY(EditDefaultsOnly)
+	float AutoRunAcceptanceRadius = 50.f;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USplineComponent> Spline;
+	
 };
