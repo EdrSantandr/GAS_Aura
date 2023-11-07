57ee00b57637c8011f24e96da0ff6f96a8205545
Eder Santander <eder@bamtang.com>
2023-11-06 19:14:59 -0500

    Tested armorpenetration and armor so it's consideredd for the calculation of the damage

--- Source/Aura/Private/AbilitySystem/ExecCalc/AuraExecCalcDamage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/AuraExecCalcDamage.cpp
@@ -9,12 +9,15 @@
 struct AuraDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor); //ThisCreate an ArmorPropertyPointer
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
 	//(1) declare the attribute you need to capture
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 
 	AuraDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target,false);
+		// Becarfull with the Source, Because we need Source value of armor penetration
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, ArmorPenetration, Source,false);
 		//(2) define the attribute you need to capture using macros
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target,false);
 	}
@@ -31,6 +34,7 @@ UAuraExecCalcDamage::UAuraExecCalcDamage()
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 	//(3) capture the attribute
 	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorPenetrationDef);
 }
 
 void UAuraExecCalcDamage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
@@ -54,13 +58,26 @@ void UAuraExecCalcDamage::Execute_Implementation(const FGameplayEffectCustomExec
 	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
 
 	//Capture Blockchance on target
-	// If BLOCK, halve the damage
 	float TargetBlockChance = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluateParameters, TargetBlockChance);
 	TargetBlockChance = FMath::Max<float>(TargetBlockChance,0.f);
 
 	const bool bBlocked = FMath::RandRange(1,100) < TargetBlockChance;
+	// If BLOCK, halve the damage
 	Damage = bBlocked ? Damage*0.5f : Damage;
+
+	//Capture the value of armor penetration
+	// Armor penetration ignores a percentage of the target's armor
+	float TargetArmor= 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, TargetArmor);
+	TargetArmor = FMath::Max<float>(TargetArmor,0.f);
+	
+	float SourceArmorPenetration = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorPenetrationDef, EvaluateParameters, SourceArmorPenetration);
+	SourceArmorPenetration = FMath::Max<float>(SourceArmorPenetration,0.f);
+
+	const float EffectiveArmor = TargetArmor *= (100 - SourceArmorPenetration * 0.25f) / 100.f;
+	Damage *= (100-EffectiveArmor * 0.333f) / 100.f;
 	
 	const FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(),EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
