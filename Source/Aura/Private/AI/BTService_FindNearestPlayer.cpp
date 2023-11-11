58b3f8a0e48f5dc7dea0f16dc032fafd1806acb5
Eder Santander <eder@bamtang.com>
2023-11-11 08:10:47 -0500

    Find the Nearestplayer done by using a service and Blackboardkeys

--- Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -2,12 +2,39 @@
 
 
 #include "AI/BTService_FindNearestPlayer.h"
-
 #include "AIController.h"
+#include "BehaviorTree/BTFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
 
 void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
-	GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Red,*AIOwner->GetName());
-	GEngine->AddOnScreenDebugMessage(2, 1.f, FColor::Black,*ActorOwner->GetName());
+
+	APawn* OwningPawn = AIOwner->GetPawn();
+	
+	const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+
+	TArray<AActor*> ActorsWithTag;
+	UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
+
+	//This way you can get the maximum value a float can have in the engine
+	float ClosestDistance = TNumericLimits<float>::Max();
+	AActor* ClosestActor = nullptr;
+	for(AActor* Actor : ActorsWithTag)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Orange, *Actor->GetName());
+		if (IsValid(Actor) && IsValid(OwningPawn))
+		{
+			const float Distance = OwningPawn->GetDistanceTo(Actor);
+			if (Distance< ClosestDistance)
+			{
+				ClosestDistance = Distance;
+				ClosestActor = Actor;
+			}
+		}
+	}
+
+	//Setup the values in the blackboardkey
+	UBTFunctionLibrary::SetBlackboardValueAsObject(this, TargetToFollowSelector,ClosestActor);
+	UBTFunctionLibrary::SetBlackboardValueAsFloat(this, DistanceToTargetSelector,ClosestDistance);
 }
